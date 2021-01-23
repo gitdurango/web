c6add7c8b5886416619dfb69fb1bb617a2452c92
Carlos <carretecarlosg@gmail.com>
2021-01-22 22:50:36 -0600

    Final?

--- admin/php/altasservicios.php
+++ admin/php/altasservicios.php
@@ -10,9 +10,9 @@ $marcamodelo=$_POST['MarcaModelo'];
 $color=$_POST['Color'];
 $enciende=$_POST['Enciende'];
 $cargador=$_POST['Cargador'];
-$daños=$_POST['Daños'];
+$danos=$_POST['Danos'];
 $comentarios=$_POST['Comentarios'];
-$contraseña=$_POST['Contraseña'];
+$contrasena=$_POST['Contrasena'];
 $descripcion=$_POST['Descripcion'];
 $estimado=$_POST['Estimado'];
 $anticipo=$_POST['Anticipo'];
@@ -22,7 +22,7 @@ $estado = 1;
 
 
 
-$sql = "INSERT INTO clientes (Nombre, Fecha, Recibe, Telefono, Equipo, MarcaModelo, Color, Enciende, Cargador, Daños, Comentarios, Contraseña, Descripcion, Estimado, Anticipo, Estado ) VALUES ('$nombre', '$fecha', '$recibe', '$telefono', '$equipo', '$marcamodelo', '$color', '$enciende', '$cargador', '$daños', '$comentarios', '$contraseña', '$descripcion', '$estimado', '$anticipo', $estado );";
+$sql = "INSERT INTO clientes (Nombre, Fecha, Recibe, Telefono, Equipo, MarcaModelo, Color, Enciende, Cargador, Danos, Comentarios, Contrasena, Descripcion, Estimado, Anticipo, Estado ) VALUES ('$nombre', '$fecha', '$recibe', '$telefono', '$equipo', '$marcamodelo', '$color', '$enciende', '$cargador', '$danos', '$comentarios', '$contrasena', '$descripcion', '$estimado', '$anticipo', $estado );";
 if(mysqli_query($enlace,$sql)){
     echo "Cliente agregado correctamente";
 }else{
--- js/clientesaltas.js
+++ js/clientesaltas.js
@@ -5,10 +5,12 @@ function insertacita(e) {
     let datos=new FormData(formulario);
     datos.append("ajax",2)
     console.log([...datos])
+    var notification = alertify.notify('Se subio de manera correcta', 'success', 5, function(){  console.log('dismissed'); });
+
     var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      alert("Se ha añadido la cita")
+      //alert("Se ha añadido la cita")
     }
   };
   xhttp.open("POST", "../admin/php/altasservicios.php", true);
